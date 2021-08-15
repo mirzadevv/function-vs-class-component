cb209fc73ff6f32c49fa1fb4f81dd213a8900a05
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-15 14:37:03 +0430

    feat: add handleIncrement function into the class component
    This feature increment the count state value

--- src/components/CounterClass.jsx
+++ src/components/CounterClass.jsx
@@ -3,10 +3,16 @@ class CounterClass extends Component {
   state = {
     count: 0,
   };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <p>This is a Counter Class {this.state.count}</p>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
