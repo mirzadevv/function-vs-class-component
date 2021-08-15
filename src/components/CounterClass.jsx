681ac51ec2d0740495a7e146ef8b838637fab5a5
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-15 14:29:05 +0430

    init: create CounterClass component
    This is a class component that returns a simple react element

---
+++ src/components/CounterClass.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CounterClass extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a Counter Class</p>
+      </div>
+    );
+  }
+}
+
+export default CounterClass;
